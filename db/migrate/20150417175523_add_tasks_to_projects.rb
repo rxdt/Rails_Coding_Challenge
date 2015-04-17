ab1d784d549484cfe61e2ecceb694556a584ac5a
Roxana del Toro <rxdeltoro@gmail.com>
2015-04-17 11:22:52 -0700

    Adds columns to Tasks table

--- db/migrate/20150417175523_add_tasks_to_projects.rb
+++ db/migrate/20150417175523_add_tasks_to_projects.rb
@@ -1,4 +1,11 @@
 class AddTasksToProjects < ActiveRecord::Migration
   def change
+    create_table :tasks do |t|
+      t.string :name
+      t.text :description
+      t.integer :difficulty, default: 0
+
+      t.timestamps
+    end
   end
 end
